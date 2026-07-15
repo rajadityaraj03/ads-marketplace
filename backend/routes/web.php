fd66c54c6d16c82ea3e5d7d191375eaf3334ec5e
unknown <rajadityaraj03@gmail.com>
2026-07-15 20:23:51 +0530

    initial setup

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+
+    try {
+
+        DB::connection()->getPdo();
+
+        return "Database Connected Successfully";
+
+    } catch (\Exception $e) {
+
+        return $e->getMessage();
+
+    }
+
 });
